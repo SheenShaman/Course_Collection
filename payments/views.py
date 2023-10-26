4909ec4dbdbf2aa20beef82546e61d78ebc24f98
Sheen <dan.80956@gmail.com>
2023-10-26 21:11:42 +0300

    add filters

--- payments/views.py
+++ payments/views.py
@@ -1,3 +1,5 @@
+from rest_framework.filters import OrderingFilter
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import generics
 
 from payments.models import Payment
@@ -7,3 +9,6 @@ from payments.serializers import PaymentsSerializer
 class PaymentsListAPIView(generics.ListAPIView):
     serializer_class = PaymentsSerializer
     queryset = Payment.objects.all()
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
+    filterset_fields = ('lesson', 'course', 'payment_method',)
+    ordering_fields = ('payment_date',)
